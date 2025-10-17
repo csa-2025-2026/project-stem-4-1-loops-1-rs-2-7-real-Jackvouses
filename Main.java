ff97595d65ea77b9d058012e8b81ff4badf47664
Jackvouses <jackvo756@gmail.com>
2025-10-17 18:18:58 +0000

    waaaaa

--- Main.java
+++ Main.java
@@ -5,5 +5,12 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    int baubau = 3;
+    while (baubau <= 25)
+    {
+      System.out.println(baubau);
+      baubau += 2;
+    }
+
   }
 }
